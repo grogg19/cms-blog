4d805e266a1714b4b60b090dfbacd2a6421c9098
grogg19 <differ@list.ru>
2021-04-18 23:32:32 +0300

    made template footer_menu.php

--- layout/footer.php
+++ layout/footer.php
@@ -25,27 +25,9 @@ use App\View;
                         </div>
                     </div>
                     <div class="col-sm-6 col-lg-3">
-
-                        <div class="dx-widget-footer">
-                            <div class="dx-widget-title">
-                                Useful Links
-                            </div>
-                            <ul class="dx-widget-list">
-                                <li><a href="/store.html">Store</a></li>
-                                <li><a href="/checkout.html">Checkout</a></li>
-                                <li><a href="/forums.html">Forums</a></li>
-                                <li><a href="/blog.html">Blog</a></li>
-                            </ul>
-                            <ul class="dx-widget-list">
-                                <li><a href="/help-center.html">Help Center</a></li>
-                                <li>
-                                    <a data-fancybox data-touch="false" data-close-existing="true" data-src="#signup" href="javascript:;">Sign Up</a>
-                                </li>
-                                <li>
-                                    <a data-fancybox data-touch="false" data-close-existing="true" data-src="#login" href="javascript:;">Log In</a>
-                                </li>
-                            </ul>
-                        </div>
+	                <?php
+                    (new View('partials.footer.footer_menu'))->render();
+	                ?>
                     </div>
                     <div class="col-sm-6 col-lg-3">
                         <div class="dx-widget-footer">
---
+++ layout/partials/footer/footer_menu.php
@@ -0,0 +1,20 @@
+<div class="dx-widget-footer">
+    <div class="dx-widget-title">
+        Useful Links
+    </div>
+    <ul class="dx-widget-list">
+        <li><a href="/store.html">Store</a></li>
+        <li><a href="/checkout.html">Checkout</a></li>
+        <li><a href="/forums.html">Forums</a></li>
+        <li><a href="/blog.html">Blog</a></li>
+    </ul>
+    <ul class="dx-widget-list">
+        <li><a href="/help-center.html">Help Center</a></li>
+        <li>
+            <a data-fancybox data-touch="false" data-close-existing="true" data-src="#signup" href="javascript:;">Sign Up</a>
+        </li>
+        <li>
+            <a data-fancybox data-touch="false" data-close-existing="true" data-src="#login" href="javascript:;">Log In</a>
+        </li>
+    </ul>
+</div>
