954a5c31234a74cea664bbcdf02efdd1ec22dfcd
grogg19 <differ@list.ru>
2021-04-19 14:54:06 +0300

    added latest_posts.php template to footer

--- layout/footer.php
+++ layout/footer.php
@@ -29,38 +29,12 @@ use App\View;
                     (new View('partials.footer.footer_menu'))->render();
 	                ?>
                     </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="dx-widget-footer">
-                            <div class="dx-widget-title">
-                                Latest Products
-                            </div>
-                            <div class="dx-widget-portfolio">
-                                <a href="/product.html">Quantial – Moving Upon Signs Moveth Lesser</a>
-                                <a href="/product.html">Sensific – Saying Beast Lesser for in Fruitful</a>
-                                <a href="/product.html">Minist – Subdue Above for Signs Dry is Have Great</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="dx-widget-footer">
-                            <div class="dx-widget-title">
-                                Latest Posts
-                            </div>
-                            <a href="/single-post.html" class="dx-widget-post">
-
-					        <span class="dx-widget-post-text">
-					            <span class="dx-widget-post-title">The Big Thing in Design</span>
-					            <span class="dx-widget-post-date">25 Sep 2018</span>
-					        </span>
-
-                            </a>
-                            <a href="/single-post.html" class="dx-widget-post">
-					        <span class="dx-widget-post-text">
-					            <span class="dx-widget-post-title">Will Coding Ever Rule the World?</span>
-					            <span class="dx-widget-post-date">24 Sep 2018</span>
-					        </span>
-                            </a>
-                        </div>
+<!--                    <div class="col-sm-6 col-lg-3">-->
+<!--                    </div>-->
+                    <div class="col-sm-12 col-lg-6">
+                        <?php
+                        (new View('partials.footer.latest_posts', (new \App\Controllers\PublicControllers\PublicPostController())->latestPosts()))->render();
+                        ?>
                     </div>
                 </div>
             </div>
---
+++ layout/partials/footer/latest_posts.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Latests posts footer partial
+ */
+?>
+<?php if(!empty($latestPosts)) { ?>
+<div class="dx-widget-footer">
+	<div class="dx-widget-title">
+        Свежие публикации
+    </div>
+    <?php foreach ($latestPosts['posts'] as $latestPost) { ?>
+    <a href="/post/<?= $latestPost->slug ?>" class="dx-widget-post">
+        <span class="dx-widget-post-text">
+            <span class="dx-widget-post-title"><?= $latestPost->title ?></span>
+            <span class="dx-widget-post-date"><?= \Helpers\getDateTime($latestPost->published_at) ?></span>
+        </span>
+    </a>
+    <?php } ?>
+</div>
+<?php } ?>
