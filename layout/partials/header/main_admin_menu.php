0bb051f136dab8382206b9020450166f10a3ac8b
grogg19 <differ@list.ru>
2021-04-21 16:05:09 +0300

    reorganized teplates menu in header.php and admin_header.php

---
+++ layout/partials/header/main_admin_menu.php
@@ -0,0 +1,81 @@
+<ul class="dx-nav dx-nav-align-left">
+
+    <li class="dx-drop-item">
+        <a href="/store.html">
+            Store
+        </a><ul class="dx-navbar-dropdown">
+
+            <li>
+                <a href="/store.html">
+                    Store
+                </a>
+            </li>
+            <li>
+                <a href="/product.html">
+                    Product
+                </a>
+            </li>
+            <li>
+                <a href="/checkout.html">
+                    Checkout
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="dx-drop-item active">
+        <a href="/blog.html">
+            Блог
+        </a><ul class="dx-navbar-dropdown">
+
+            <li class=" active">
+                <a href="/blog.html">
+                    Блог
+                </a>
+            </li>
+            <li>
+                <a href="/admin/blog/posts/create">
+                    Создать пост
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="dx-drop-item">
+        <a href="/help-center.html">
+            Страницы
+        </a><ul class="dx-navbar-dropdown">
+
+            <li>
+                <a href="/admin/static-pages">
+                    Список статических страниц
+                </a>
+            </li>
+            <li class="dx-drop-item">
+                <a href="/admin/static-pages/add">
+                    Добавить
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="dx-drop-item">
+        <a href="/account.html">
+            Account
+        </a><ul class="dx-navbar-dropdown">
+
+            <li>
+                <a href="/account.html">
+                    Account
+                </a>
+            </li>
+            <li>
+                <a href="/account-licenses.html">
+                    Licenses
+                </a>
+            </li>
+            <li>
+                <a href="/account-settings.html">
+                    Settings
+                </a>
+            </li>
+        </ul>
+    </li>
+</ul>
