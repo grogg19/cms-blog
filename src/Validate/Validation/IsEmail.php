daf13ad59bbc17f0c87f1cf16255e3abf7f62a06
grogg19 <differ@list.ru>
2021-04-27 15:12:28 +0300

    Добавлена валидация Email

---
+++ src/Validate/Validation/IsEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Validate\Validation;
+
+use App\Validate\Validation;
+
+/**
+ * Class IsEmail
+ * @package App\Validate\Validation
+ */
+class IsEmail extends Validation
+{
+    /**
+     * IsEmail constructor.
+     * @param string $data
+     */
+    public function __construct(string $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Метод вызывает реализацию
+     * @return bool
+     */
+    public function run(): bool
+    {
+        return $this->isEmail();
+    }
+
+    /**
+     * @return bool
+     */
+    private function isEmail(): bool
+    {
+        if(is_string(filter_var($this->data, FILTER_VALIDATE_EMAIL))) {
+            $this->message = "Формат Email неверный";
+            return false;
+        }
+        return true;
+    }
+}
