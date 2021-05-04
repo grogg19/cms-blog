9162a273344ec502e304c7cb29188444f21237ab
grogg19 <differ@list.ru>
2021-05-04 23:06:40 +0300

    fix tabs

--- layout/forms/editor.php
+++ layout/forms/editor.php
@@ -3,17 +3,17 @@
  * Шаблон элемента EDITOR
  */
 ?>
-                                <div class="dx-form-group div-<?=key($element)?> form-element">
-                                    <?php if($element[key($element)]['label']) { ?>
-                                        <label for="form_editor" ><?=$element[key($element)]['label']?>:</label>
-                                    <?php } ?>
-                                    <div class="dx-editor-quill">
-                                        <div id="form_editor" name="form_editor" <?php if (isset($element[key($element)]['options'])) {
-                                            foreach ($element[key($element)]['options'] as $key => $value) {
-                                                echo ' ' .$key . '="' . $value . '" ';
-                                            }
-                                        } ?> ><?php if (!empty($valueAttribute)) { ?><?=$valueAttribute?>
-                                            <?php } ?></div>
-                                    </div>
-                                </div>
+<div class="dx-form-group div-<?=key($element)?> form-element">
+    <?php if($element[key($element)]['label']) { ?>
+        <label for="form_editor" ><?=$element[key($element)]['label']?>:</label>
+    <?php } ?>
+    <div class="dx-editor-quill">
+        <div id="form_editor" name="form_editor" <?php if (isset($element[key($element)]['options'])) {
+            foreach ($element[key($element)]['options'] as $key => $value) {
+                echo ' ' .$key . '="' . $value . '" ';
+            }
+        } ?> ><?php if (!empty($valueAttribute)) { ?><?=$valueAttribute?>
+            <?php } ?></div>
+    </div>
+</div>
 
