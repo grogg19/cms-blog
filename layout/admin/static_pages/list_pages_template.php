384ea3280e3d4e048e6a0f872f0c0fb4747923c0
grogg19 <differ@list.ru>
2021-05-07 23:39:45 +0300

    added buttons edit & delete

--- layout/admin/static_pages/list_pages_template.php
+++ layout/admin/static_pages/list_pages_template.php
@@ -6,7 +6,7 @@
 <div class="dx-box-5 pb-100 bg-grey-6">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-10 col-xl-7">
+            <div class="col-lg-10 col-xl-10">
                 <div class="dx-box-decorated">
                     <div class="dx-box-content">
                         <h2 class="h6 mt-5 mb-5"><?= !empty($title) ? $title : ''?></h2>
@@ -15,11 +15,16 @@
 <!--	                    </div>-->
 	                    <?php if(!empty($pages)) { ?>
 		                    <div class="dx-separator"></div>
+		                    <form class="dx-form" name="form_edit_static_pages" id="form_edit_static_pages" >
+			                    <input type="hidden" name="_token" value="<?=(!empty($token) ? $token : '')?>">
 		                    <?php foreach ($pages as $page) { ?>
-			                    <ul class="text-left mb-6 mt-6">
-				                    <li><a href="/admin/static-pages<?= $page->getParameter('url');?>/edit"><?= $page->getParameter('title') ?> url: <?= $page->getParameter('url') ?></a></li>
-			                    </ul>
+			                    <div class="row mb-6 mt-6" >
+			                        <div class="col-lg-10"><?= $page->getParameter('title') ?></div>
+			                        <div class="col-lg-1"><a href="/admin/static-pages/edit" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/edit"><i class="fas fa-edit"></i></a></div>
+			                        <div class="col-lg-1"><a href="/admin/static-pages/delete" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/delete"><i class="fas fa-times"></i></a></div>
+			                    </div>
 			                <?php } ?>
+		                    </form>
 	                    <?php } ?>
                     </div>
                 </div>
