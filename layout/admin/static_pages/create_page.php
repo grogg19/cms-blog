f22d01d6e549f457f7b14d68cfa9e8d43d003a2f
grogg19 <differ@list.ru>
2021-05-08 16:24:22 +0300

    delete empty strings

--- layout/admin/static_pages/create_page.php
+++ layout/admin/static_pages/create_page.php
@@ -11,32 +11,27 @@
                 <div class="dx-box dx-box-decorated">
                     <div class="dx-box-content">
                         <h2 class="h6 mb-6">Создание новой страницы</h2>
-
-
                         <!-- START: Breadcrumbs -->
                         <ul class="dx-breadcrumbs text-left dx-breadcrumbs-dark mnb-6 fs-14">
-
-                            <li><a href="/admin">Раздел администрирования</a></li>
-
+	                        <li><a href="/admin/static-pages">Статические страницы</a></li>
                             <li>Создание новой страницы</li>
-
                         </ul>
                         <!-- END: Breadcrumbs -->
-
                     </div>
                     <div class="dx-separator"></div>
                     <form class="dx-form" name="form_create_page" id="form_create_page" action="<?= (isset($form['action'])) ? $form['action'] : ""?>">
                         <input type="hidden" name="_token" value="<?=(!empty($token) ? $token : '')?>">
                         <div class="dx-box-content">
                             <?php
-                            (new \App\FormRenderer($form['fields']))->render();
+                            if(!empty($form)) {
+                                (new \App\FormRenderer($form['fields']))->render();
+                            }
                             ?>
                         </div>
                     </form>
 	                <div class="dx-box-content">
 		                <button class="dx-btn dx-btn-lg" type="submit" name="button" id="save_button" data-form="form_create_page">Сохранить</button>
 	                </div>
-
                 </div>
             </div>
         </div>
