0d1bf325119cf0f3bca2cbf7b5e412301b50ad66
grogg19 <differ@list.ru>
2021-05-08 20:28:26 +0300

    changes in template

--- layout/admin/static_pages/list_pages_template.php
+++ layout/admin/static_pages/list_pages_template.php
@@ -18,16 +18,19 @@
 		                    <form class="dx-form" name="form_edit_static_pages" id="form_edit_static_pages" >
 			                    <input type="hidden" name="_token" value="<?=(!empty($token) ? $token : '')?>">
 			                    <div class="row mb-6 mt-6" >
-				                    <div class="col-lg-10"><b>Заголовок</b></div>
-				                    <div class="col-lg-1 text-center"><b>Изм.</b></div>
-				                    <div class="col-lg-1 text-center"><b>Уд.</b></div>
+				                    <div class="col-10 col-10"><b>Заголовок</b></div>
+				                    <div class="col-1 col-1 text-center"><i class="fas fa-edit"></i></div>
+				                    <div class="col-1 col-1 text-center"><i class="fas fa-trash-alt"></i></div>
 				                </div>
 			                    <div class="dx-separator"></div>
 			                    <div class="row mb-6 mt-6" >
 		                    <?php foreach ($pages as $page) { ?>
-			                        <div class="col-lg-10"><?= $page->getParameter('title') ?></div>
-			                        <div class="col-lg-1 text-center"><a href="/admin/static-pages/edit" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/edit"><i class="fas fa-edit"></i></a></div>
-			                        <div class="col-lg-1 text-center"><a href="/admin/static-pages/delete" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/delete"><i class="fas fa-times"></i></a></div>
+			                        <div class="col-10 mb-6 mt-6" >
+				                        <p class="m-0"><i class="far fa-file-alt"></i> <?= $page->getParameter('title') ?></p>
+				                        <p class="small op-7"><?= $page->getParameter('url'); ?></p>
+			                        </div>
+			                        <div class="col-1 text-center mb-6 mt-6"><a href="/admin/static-pages/edit" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/edit" title="Редактировать страницу"><i class="fas fa-pencil-alt"></i></a></div>
+			                        <div class="col-1 text-center mb-6 mt-6"><a href="/admin/static-pages/delete" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/delete" title="Удалить страницу"><i class="fas fa-times"></i></a></div>
 			                <?php } ?>
 			                    </div>
 		                    </form>
