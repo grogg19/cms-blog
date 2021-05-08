171103ac016e796894a8c88e1cc96b910d66c695
grogg19 <differ@list.ru>
2021-05-08 16:59:44 +0300

    добавлены подписи к столбцам

--- layout/admin/static_pages/list_pages_template.php
+++ layout/admin/static_pages/list_pages_template.php
@@ -10,20 +10,23 @@
                 <div class="dx-box-decorated">
                     <div class="dx-box-content">
                         <h2 class="h6 mt-5 mb-5"><?= !empty($title) ? $title : ''?></h2>
-<!--	                    <div class="mt-10 mb-10">-->
+	                    <div class="mt-10 mb-10">
 		                    <a href="/admin/static-pages/add" ><i class="fas fa-plus"></i> Добавить страницу</a>
-<!--	                    </div>-->
+	                    </div>
 	                    <?php if(!empty($pages)) { ?>
 		                    <div class="dx-separator"></div>
 		                    <form class="dx-form" name="form_edit_static_pages" id="form_edit_static_pages" >
 			                    <input type="hidden" name="_token" value="<?=(!empty($token) ? $token : '')?>">
-		                    <?php foreach ($pages as $page) { ?>
 			                    <div class="row mb-6 mt-6" >
+				                    <div class="col-lg-10"><b>Заголовок</b></div>
+				                    <div class="col-lg-1 text-center"><b>Изм.</b></div>
+				                    <div class="col-lg-1 text-center"><b>Уд.</b></div>
+		                    <?php foreach ($pages as $page) { ?>
 			                        <div class="col-lg-10"><?= $page->getParameter('title') ?></div>
-			                        <div class="col-lg-1"><a href="/admin/static-pages/edit" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/edit"><i class="fas fa-edit"></i></a></div>
-			                        <div class="col-lg-1"><a href="/admin/static-pages/delete" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/delete"><i class="fas fa-times"></i></a></div>
-			                    </div>
+			                        <div class="col-lg-1 text-center"><a href="/admin/static-pages/edit" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/edit"><i class="fas fa-edit"></i></a></div>
+			                        <div class="col-lg-1 text-center"><a href="/admin/static-pages/delete" data-type="action" data-value="<?=$page->getFileName()['name']?>" data-action="/admin/static-pages/delete"><i class="fas fa-times"></i></a></div>
 			                <?php } ?>
+			                    </div>
 		                    </form>
 	                    <?php } ?>
                     </div>
