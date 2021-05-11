fa301ab5540913e077c4e7ac0d5cc2559f111a33
grogg19 <differ@list.ru>
2021-05-11 16:02:20 +0300

    some refactored

--- layout/partials/toast_main.php
+++ layout/partials/toast_main.php
@@ -1,28 +1,12 @@
 <?php
 use App\View;
-?>
-<h1>Toasts</h1>
-<div class="toast__actions">
-    <button class="toast__trigger">Info Toast</button>
-    <button class="toast__trigger">Warning Toast</button>
-    <button class="toast__trigger">Success Toast</button>
-</div>
 
+if (!empty($typeToast) && !empty($dataToast)) { ?>
 <section class="toast__container">
-
-<?php
-    (new View('partials.toasts.info', [
-        'title' => (!empty($title)) ? $title : ''
+    <?php
+    (new View('partials.toasts.' . $typeToast, [
+        'data' => $dataToast
     ]))->render();
-
-    (new View('partials.toasts.warning', [
-        'title' => (!empty($title)) ? $title : ''
-    ]))->render();
-
-    (new View('partials.toasts.success', [
-        'title' => (!empty($title)) ? $title : ''
-    ]))->render();
-?>
-
-
+    ?>
 </section>
+<?php } ?>
