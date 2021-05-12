9b6d085de7483ebd83be5f6195817b4ac0193965
grogg19 <differ@list.ru>
2021-05-12 20:56:01 +0300

    upgrade template

--- layout/admin/list_posts.php
+++ layout/admin/list_posts.php
@@ -10,7 +10,9 @@ $posts = !empty($posts) ? $posts : [];
 <div class="dx-box-1 pb-100 bg-grey-6">
     <div class="container">
         <div class="row vertical-gap md-gap">
-            <div class="col-lg-12">
+            <div class="col-12">
+	            <h2 class="m-0"><i class="far fa-list-alt"></i> Список статей блога</h2>
+	            <div class="dx-separator mb-10 mt-0"></div>
                 <?php
                 foreach ($posts as $post) {
                     ?>
@@ -18,16 +20,24 @@ $posts = !empty($posts) ? $posts : [];
                         <div class="dx-blog-item-cont">
                             <h2 class="h3 dx-blog-item-title"><a href="/admin/blog/posts/<?= $post->id ?>/edit"><?= $post->title ?></a></h2>
                             <ul class="dx-blog-item-info">
-                                <li>Опубликовано: <?= getDateTime($post->published_at) ?></li>
+                                <li class="d-block"><i class="far fa-clock" title="Опубликовано"></i> <?= getDateTime($post->published_at) ?></li>
                                 <?php
                                 // Условие при котором показывается автор статьи
                                 ?>
-                                <li>Автор: <a href="#"><?=$post->user->first_name?> <?=$post->user->last_name?></a></li>
+                                <li class="d-block"><i class="fas fa-user" title="Автор"></i> <a href="#"><?=$post->user->first_name?> <?=$post->user->last_name?></a></li>
                                 <?php
                                 // --------
                                 ?>
                             </ul>
-                            <a href="/admin/blog/posts/<?= $post->id ?>/edit" class="dx-btn dx-btn-lg">Редактировать</a>
+	                        <div class="row">
+		                        <div class="col-6">
+			                        <a href="/admin/blog/posts/<?= $post->id ?>/edit" class="btn btn-primary"><i class="fas fa-pencil-alt mr-7"></i> Редактировать</a>
+		                        </div>
+		                        <div class="col-6 text-right">
+			                        <a class="btn btn-secondary" href="#" role="button"><i class="far fa-trash-alt"></i></a>
+		                        </div>
+	                        </div>
+
                         </div>
                     </div>
                 <?php } ?>
