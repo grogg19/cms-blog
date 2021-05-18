470ac542e641e1f8eca2465f4e49c709adda0705
grogg19 <differ@list.ru>
2021-05-18 16:36:34 +0300

    defined $post & $form

--- layout/admin/edit_post.php
+++ layout/admin/edit_post.php
@@ -3,6 +3,12 @@
  * Шаблон
  * Редактирование поста
  */
+
+use App\FormRenderer;
+
+$post = !empty($post) ? $post : [];
+$form = !empty($form) ? $form : [];
+
 ?>
 <div class="dx-box-5 pb-100 bg-grey-6">
     <div class="container">
@@ -11,34 +17,25 @@
                 <div class="dx-box dx-box-decorated">
                     <div class="dx-box-content">
                         <h2 class="h6 mb-6">Редактирование поста</h2>
-
-
                         <!-- START: Breadcrumbs -->
                         <ul class="dx-breadcrumbs text-left dx-breadcrumbs-dark mnb-6 fs-14">
-
-                            <li><a href="help-center.html">Раздел администрирования</a></li>
-
+                            <li><a href="/admin">Раздел администрирования</a></li>
                             <li>Редактирование поста</li>
-
                         </ul>
                         <!-- END: Breadcrumbs -->
-
                     </div>
                     <div class="dx-separator"></div>
                     <form class="dx-form" name="form_edit_post" id="form_edit_post" action="<?= (isset($form['action'])) ? $form['action'] : ""?>">
                         <input type="hidden" name="_token" value="<?= !empty($token) ? $token : ''?>">
-
                         <div class="dx-box-content">
                             <input type="hidden" name="idPost" id="idPost" value="<?=!empty($post->id) ? $post->id : ''?>">
                             <?php
-                            (new \App\FormRenderer($form['fields']))->render($post);
+                            (new FormRenderer($form['fields']))->render($post);
                             ?>
-
                         </div>
                     </form>
                     <div class="dx-box-content pt-0">
                         <!-- START: Dropzone
-
                             Additional Attributes:
                             data-dropzone-action
                             data-dropzone-maxMB
@@ -57,7 +54,7 @@
                         </form>
                         <div class="row justify-content-between vertical-gap dx-dropzone-attachment">
                             <div class="col-auto dx-dropzone-attachment-add">
-                                <label class="mb-0" class="mnt-7"><span class="icon fas fa-paperclip mr-10"></span><span>Добавить изображение</span></label>
+                                <label class="mb-0 mnt-7"><span class="icon fas fa-paperclip mr-10"></span><span>Добавить изображение</span></label>
                             </div>
                             <div class="col-auto dx-dropzone-attachment-btn">
                                 <button class="dx-btn dx-btn-lg" type="submit" name="button" id="save_button" data-form="form_edit_post">Сохранить</button>
