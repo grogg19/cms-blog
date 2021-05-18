12ce3e284d1e83c02be4ad7fdec557ba5e7ac570
grogg19 <differ@list.ru>
2021-05-18 14:26:00 +0300

    fixed bug with toasts & slugify

--- layout/partials/signup.php
+++ layout/partials/signup.php
@@ -7,7 +7,6 @@
 $fields = !empty($fields) ? $fields : [];
 ?>
 <!-- Start SignUp Form -->
-<div id="messageToast"></div>
 <div class="dx-popup dx-popup-signin" id="block_signup" >
     <button type="button" data-fancybox-close class="fancybox-button fancybox-close-small" title="Close"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path d="M13 12l5-5-1-1-5 5-5-5-1 1 5 5-5 5 1 1 5-5 5 5 1-1z"></path></svg></button>
     <div class="dx-signin-content dx-signin text-center">
@@ -33,6 +32,7 @@ $fields = !empty($fields) ? $fields : [];
             </div>
         </form>
     </div>
+	<div id="messageToast"></div>
 </div>
 
 <!-- End Login Form -->
