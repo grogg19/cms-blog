342c00585cf2cc8065ad2abc074ed6f92fba416d
grogg19 <differ@list.ru>
2021-05-20 16:38:23 +0300

    fix sql-dump

--- migrations/0004_add_blog_posts_images.sql
+++ migrations/0004_add_blog_posts_images.sql
@@ -1,15 +1,26 @@
-create table if not exists blog_posts_images
-(
-    id        int unsigned auto_increment
-        primary key,
-    file_name varchar(255) not null,
-    post_id   int unsigned not null,
-    sort      int unsigned null,
-    constraint blog_posts_images_blog_posts_id_fk
-        foreign key (post_id) references blog_posts (id)
-            on update cascade on delete cascade
-)
-    engine = innodb
-    auto_increment = 1
-    character set utf8
-    collate utf8_unicode_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blog_posts_images`
+--
+
+DROP TABLE IF EXISTS `blog_posts_images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `blog_posts_images` (
+                                     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                                     `file_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+                                     `post_id` int(10) unsigned NOT NULL,
+                                     `sort` int(10) unsigned DEFAULT NULL,
+                                     PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
