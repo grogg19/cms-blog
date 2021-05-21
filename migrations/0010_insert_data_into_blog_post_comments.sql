873d701c11b16fe64d1ebac66b1340b10dccd7e0
grogg19 <differ@list.ru>
2021-05-21 16:14:03 +0300

    добавлены данные для blog_post_comments

---
+++ migrations/0010_insert_data_into_blog_post_comments.sql
@@ -0,0 +1,15 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+INSERT INTO cms.blog_post_comments (id, post_id, user_id, content, has_moderated, created_at, updated_at) VALUES (1, 12, 1, 'Автор хорошо раскрыл тему, респект!', 1, '2021-05-21 16:07:07', '2021-05-21 16:07:15');
+INSERT INTO cms.blog_post_comments (id, post_id, user_id, content, has_moderated, created_at, updated_at) VALUES (2, 12, 8, 'А по мне, так стоило еще поработать', 1, '2021-05-21 16:09:07', '2021-05-21 16:09:10');
