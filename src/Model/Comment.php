f94187af71430ff564add24b5fb3c6403e891c8c
grogg19 <differ@list.ru>
2021-05-21 16:35:44 +0300

    добавлена модель Comment

---
+++ src/Model/Comment.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Модель Comment
+ */
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'blog_post_comments';
+    protected $primaryKey = 'id';
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $fillable = ['post_id', 'user_id'];
+
+    public $rules = [
+        'content' => 'required'
+    ];
+
+    /**
+     * @return array
+     */
+    public function getRules(): array
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
