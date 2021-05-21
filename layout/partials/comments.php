d3c2c04ee3cd70019b458d5cb1fb00d7b1d2ce59
grogg19 <differ@list.ru>
2021-05-22 00:51:09 +0300

    добавлен шаблон comments

---
+++ layout/partials/comments.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Controllers\UserController;
+use function Helpers\getDateTime;
+
+$avatarPath = (new UserController())->getUserAvatarPath();
+?>
+<div class="dx-box mt-55">
+    <?php
+    if(!empty($comments)) {
+    ?>
+    <h2 class="h4 mb-45">Комментарии:</h2>
+	<?php foreach ($comments as $comment) { ?>
+    <div class="dx-comment">
+        <div>
+            <div class="dx-comment-img">
+                <?= (empty($comment->user->avatar)) ? '<img src="/layout/assets/images/avatar-1.png" alt="">' : '<img src="' . $avatarPath . $comment->user->avatar . '" alt="">' ?>
+            </div>
+            <div class="dx-comment-cont">
+                <a href="#" class="dx-comment-name"><?= $comment->user->first_name ?> <?= $comment->user->last_name ?></a>
+                <div class="dx-comment-text">
+                    <p class="mb-0"><?= htmlspecialchars($comment->content) ?></p>
+                </div>
+                <div class="dx-comment-date"><i class="far fa-clock"></i> <?= getDateTime($comment->created_at)?></div>
+            </div>
+        </div>
+    </div>
+		<?php }?>
+    <?php }	?>
+    <form action="#" class="dx-form mt-50">
+        <div class="row vertical-gap">
+            <div class="col-md-6">
+                <input class="form-control form-control-style-3" type="text" placeholder="Имя">
+            </div>
+            <div class="col-md-6">
+                <input class="form-control form-control-style-3" type="text" placeholder="Вымя">
+            </div>
+            <div class="col-12">
+                <textarea class="form-control form-control-style-3" name="name" rows="8" cols="80" placeholder="Ваш комментарий..."></textarea>
+            </div>
+            <div class="col-12">
+                <button type="button" name="button" class="dx-btn dx-btn-lg">Оставить комментарий</button>
+            </div>
+        </div>
+    </form>
+</div>
--- layout/post.php
+++ layout/post.php
@@ -6,6 +6,7 @@ use App\View;
 
 $post = !empty($post) ? $post : null;
 $imgPath = !empty($imgPath) ? $imgPath : '';
+$comments = !empty($post->comments) ? $post->comments : [];
 ?>
 <div class="dx-box-1 pb-100 bg-grey-6">
     <div class="container">
@@ -62,70 +63,13 @@ $imgPath = !empty($imgPath) ? $imgPath : '';
                         </ul>
                     </div>
                 </div>
-                <div class="dx-box mt-55">
-                    <h2 class="h4 mb-45">Комментарии:</h2>
-                    <div class="dx-comment">
-                        <div>
-                            <div class="dx-comment-img">
-                                <img src="/layout/assets/images/avatar-1.png" alt="">
-                            </div>
-                            <div class="dx-comment-cont">
-                                <a href="#" class="dx-comment-name">John Leonard</a>
-                                <a href="#" class="dx-comment-reply">Ответить</a>
-                                <div class="dx-comment-text">
-                                    <p class="mb-0">Nullam ac dui et purus malesuada gravida id fermentum orci. In eu ipsum quis urna hendrerit condimentum vitae a mauris. In congue turpis purus, vitae tempus ante id. Donec orci arcu, sagittis ut finibus vitae.</p>
-                                </div>
-                                <div class="dx-comment-date">12 Feb 2018 7:40 pm</div>
-                            </div>
-                        </div>
-                        <div class="dx-comment">
-                            <div>
-                                <div class="dx-comment-img">
-                                    <img src="/layout/assets/images/avatar-2.png" alt="">
-                                </div>
-                                <div class="dx-comment-cont">
-                                    <a href="#" class="dx-comment-name">Mercy Shields</a>
-                                    <a href="#" class="dx-comment-reply">Reply</a>
-                                    <div class="dx-comment-text">
-                                        <p class="mb-0">In at nunc sodales lorem blandit egestas. Suspendisse molestie fringilla purus, eget sagittis nunc ornare sit amet. Praesent vitae ligula eu massa rutrum hendrerit non facilisis nulla interdum ut.</p>
-                                    </div>
-                                    <div class="dx-comment-date">14 Feb 2018 8:29 am</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="dx-comment">
-                        <div>
-                            <div class="dx-comment-img">
-                                <img src="/layout/assets/images/avatar-3.png" alt="">
-                            </div>
-                            <div class="dx-comment-cont">
-                                <a href="#" class="dx-comment-name">Maria Anthony</a>
-                                <a href="#" class="dx-comment-reply">Reply</a>
-                                <div class="dx-comment-text">
-                                    <p class="mb-0">Donec libero sapien, dapibus id blandit sit amet, iaculis sed ligula. Morbi cursus maximus elementum. Phasellus viverra lacinia sapien sagittis tristique.</p>
-                                </div>
-                                <div class="dx-comment-date">18 Feb 2018 7:13 pm</div>
-                            </div>
-                        </div>
-                    </div>
-                    <form action="#" class="dx-form mt-50">
-                        <div class="row vertical-gap">
-                            <div class="col-md-6">
-                                <input class="form-control form-control-style-3" type="text" placeholder="Имя">
-                            </div>
-                            <div class="col-md-6">
-                                <input class="form-control form-control-style-3" type="text" placeholder="Вымя">
-                            </div>
-                            <div class="col-12">
-                                <textarea class="form-control form-control-style-3" name="name" rows="8" cols="80" placeholder="Ваш комментарий..."></textarea>
-                            </div>
-                            <div class="col-12">
-                                <button type="button" name="button" class="dx-btn dx-btn-lg">Оставить комментарий</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+	            <?php
+	            	/** Блок комментариев к посту*/
+                    (new View('partials.comments', [
+                    		'comments' => $comments,
+	                        'user' => $post->user
+                    ]))->render();
+	            ?>
             </div>
             <div class="col-lg-4">
                 <div class="dx-sticky dx-sidebar" data-sticky-offsetTop="120" data-sticky-offsetBot="40">
