56dbc2fd20a01830b2fa5e57a893d7865a443dd6
grogg19 <differ@list.ru>
2021-05-21 16:02:04 +0300

    добавлен FK к blog_posts

--- migrations/0009_add_blog_post_comments.sql
+++ migrations/0009_add_blog_post_comments.sql
@@ -1,12 +1,14 @@
-create table if not exists blog_post_comments
+create table blog_post_comments
 (
-    id int auto_increment,
-    post_id int not null,
-    user_id int not null,
+    id int unsigned auto_increment
+        primary key,
+    post_id int unsigned not null,
+    user_id int unsigned not null,
     content text not null,
-    has_moderated bool default 0 null,
+    has_moderated tinyint(1) default 0 null,
     created_at timestamp null,
     updated_at timestamp null,
-    constraint blog_post_comments_pk
-    primary key (id)
+    constraint blog_post_comments_blog_posts_id_fk
+        foreign key (post_id) references blog_posts (id)
+            on delete cascade
 );
