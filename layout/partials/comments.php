805fad0d7b0ba46adfb3563f5fb48053fdf6899a
grogg19 <differ@list.ru>
2021-05-24 12:39:04 +0300

    метка немодерированного коммента заменена на текст

--- layout/partials/comments.php
+++ layout/partials/comments.php
@@ -20,17 +20,13 @@ $comments = !empty($comments) ? $comments : [];
 	        <div class="dx-comment-img"><?= (empty($comment->user->avatar)) ? '<img src="/layout/assets/images/avatar-1.png" alt="">' : '<img src="' . $avatarPath . $comment->user->avatar . '">' ?></div>
             <div class="dx-comment-cont">
                 <a href="#" class="dx-comment-name"><?= $comment->user->first_name ?> <?= $comment->user->last_name ?></a>
+                <?php if(!$comment->has_moderated) { ?>
+		            <div class="small op-5">Не проверено модератором</div>
+                <?php } ?>
                 <div class="dx-comment-text">
                     <p class="mb-0"><?= htmlspecialchars($comment->content) ?></p>
                 </div>
-	            <div class="row">
-                    <div class="dx-comment-date col-6"><i class="far fa-clock"></i> <?= getDateTime($comment->created_at)?></div>
-	                <div class="dx-comment-date col-6 text-right">
-		                <?php if(!$comment->has_moderated) { ?>
-			            <i class="fas fa-ban" title="Не проверено модератором"></i>
-		                <?php } ?>
-	                </div>
-	            </div>
+	            <div class="dx-comment-date"><i class="far fa-clock"></i> <?= getDateTime($comment->created_at)?></div>
             </div>
         </div>
     </div>
