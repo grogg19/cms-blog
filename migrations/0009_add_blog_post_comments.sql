832fb1c3f6537f1df9b71c1c7400ea3f2ec77e74
grogg19 <differ@list.ru>
2021-05-26 11:08:01 +0300

    added drop command

--- migrations/0009_add_blog_post_comments.sql
+++ migrations/0009_add_blog_post_comments.sql
@@ -1,3 +1,21 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS blog_post_comments;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+--
+-- Table structure for table `blog_post_comments`
+--
 create table blog_post_comments
 (
     id int unsigned auto_increment
@@ -11,8 +29,8 @@ create table blog_post_comments
     constraint blog_post_comments_blog_posts_id_fk
         foreign key (post_id) references blog_posts (id)
             on delete cascade,
-
     constraint blog_post_comments_blog_users_id_fk
     foreign key (user_id) references users (id)
     on delete cascade
 );
+/*!40101 SET character_set_client = @saved_cs_client */;
