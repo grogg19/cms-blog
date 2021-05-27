643468f96e27e47ecad2d4ae65659b1137e03cfa
grogg19 <differ@list.ru>
2021-05-27 14:40:01 +0300

    добавлен шаблон списка комментариев

---
+++ layout/admin/comments/list.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Список статических страниц - шаблон
+ */
+
+use function Helpers\getDateTime;
+/**
+ * @var array $comments
+ * @var string $avatarPath
+ * @var string $token
+ */
+?>
+<div class="dx-box-5 pb-100 bg-grey-6">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-11 col-xl-11">
+                <div class="dx-box-decorated">
+                    <div class="dx-box-content">
+	                    <div class="col pl-10 pr-10 d-none d-sm-block">
+		                    <div class="container">
+			                    <div class="row align-items-center justify-content-between vertical-gap mnt-20 sm-gap mb-30">
+				                    <div class="col-auto">
+					                    <h2 class="h4 mb-0 mt-0"><?= !empty($title) ? $title : ''?></h2>
+				                    </div>
+				                    <div class="col pl-10 pr-10 d-none d-sm-block">
+					                    <div class="dx-separator ml-0 mr-0" id="comments-block"></div>
+					                    <input type="hidden" name="_token" value="<?= $token ?>">
+				                    </div>
+			                    </div>
+		                        <?php foreach ($comments as $comment) { ?>
+			                    <div class="dx-ticket-item dx-comment
+				                     <?= ($comment->has_moderated) ? 'dx-ticket-open' : 'dx-ticket-closed' ?>
+				                     dx-block-decorated" data-comment-id="<?= $comment->id ?>">
+								    <span class="dx-ticket-img">
+								        <img src="<?= $avatarPath . DIRECTORY_SEPARATOR .$comment->user->avatar ?>"
+								             alt="<?= $comment->user->first_name ?> <?= $comment->user->last_name ?>">
+								    </span>
+				                    <span class="dx-ticket-cont">
+								        <span class="dx-ticket-name">
+								            <?= $comment->user->role->name ?>: <?= $comment->user->first_name ?> <?= $comment->user->last_name ?>
+								        </span>
+							        <span class="dx-ticket-title h5">
+							            <?=$comment->post->title?>
+							        </span>
+							        <ul class="dx-ticket-info">
+							            <li>Дата комментирования: <?= getDateTime($comment->created_at)?></li>
+							            <li>
+								            <a href="#" class="btn-comment-content" data-id="<?= $comment->id ?>">Посмотреть</a>
+							            </li>
+							            <li>
+								            <?php if($comment->has_moderated) { ?>
+								            <a href="/admin/posts/comments/reject" data-type="request" data-value="<?= $comment->id ?>" data-field="commentId">Отклонить</a>
+								            <?php } else { ?>
+									        <a href="/admin/posts/comments/approve" data-type="request" data-value="<?= $comment->id ?>" data-field="commentId">Одобрить</a>
+								            <?php } ?>
+							            </li>
+							        </ul>
+					                <div class="mt-30 mb-30 comment-content comment-hidden"  data-content-id="<?= $comment->id ?>"><?= $comment->content ?></div>
+								    </span>
+					                    <span class="dx-ticket-status">
+								        <?= ($comment->has_moderated) ? 'Проверено' : 'Не проверено' ?>
+								    </span>
+			                    </div>
+		                        <?php } ?>
+		                    </div>
+		                    <div id="messageToast"></div>
+	                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
