3d46d5e438d7c9e03aa0ea439912ea2f87da996a
grogg19 <differ@list.ru>
2021-06-16 15:37:44 +0300

    changed some data in comments & added PROCESS privilege

--- db-init.sh
+++ db-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 service mysql start
 mysql -u root -e "CREATE USER 'dbuser'@'%' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, DROP, REFERENCES, SHOW VIEW ON *.* TO 'dbuser'@'%';"
+mysql -u root -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, DROP, REFERENCES, PROCESS, SHOW VIEW ON *.* TO 'dbuser'@'%';"
 mysql -u root -e "CREATE DATABASE cms;"
